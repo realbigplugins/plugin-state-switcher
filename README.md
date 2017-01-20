2755a0a92ddba35097fdaafd7163e5750a4d3cf5
joelworsham <joel.worsham@gmail.com>
2017-01-20 10:21:28 -0500

    v0.1.0 - Initial push

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plugin State Switcher
+
+This plugin is designed to create different "sets" of plugin states. So, activating/deactivating different sets of plugins. Mostly helpful for develepment environemnts.
---
+++ plugin-state-switcher.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Plugin State Switcher
+ * Description: Helps quickly switching between plugin sets.
+ * Version 0.1.0
+ * Author: Joel Worsham
+ * Author URI: http://realbigmarketing.com
+ */
+
+defined( 'ABSPATH' ) || die;
